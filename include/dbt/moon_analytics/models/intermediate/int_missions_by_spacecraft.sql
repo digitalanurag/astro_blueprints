b2de6c0e8ae6507e635f649a2f846306857a1298
digitalanurag <anurag.pandey@thedigitalgroup.com>
2026-08-14 15:45:39 +0530

    hi

--- include/dbt/moon_analytics/models/intermediate/int_missions_by_spacecraft.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    spacecraft_type,
-    COUNT(*) AS total_missions,
-    COUNT(*) FILTER (WHERE outcome = 'Success') AS successful_missions,
-    COUNT(*) FILTER (WHERE outcome != 'Success') AS failed_missions
-FROM {{ ref('stg_moon_missions') }}
-GROUP BY spacecraft_type
